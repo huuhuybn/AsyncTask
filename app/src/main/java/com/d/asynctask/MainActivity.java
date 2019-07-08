7e8226078565de4175b678c50d2c4b5231162554
HuyNguyen <huuhuybn@gmail.com>
2019-07-08 10:38:37 +0700

    Initial commit

---
+++ app/src/main/java/com/d/asynctask/MainActivity.java
@@ -0,0 +1,78 @@
+package com.d.asynctask;
+
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView tvTitle;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+        tvTitle = findViewById(R.id.tvTitle);
+
+        MyTask myTask = new MyTask(tvTitle);
+
+        myTask.execute("http://docbao.vn");
+
+
+        MyTask.OnLoadCompletedListener onLoadCompletedListener = new MyTask.OnLoadCompletedListener() {
+            @Override
+            public void onFinished(String result) {
+                tvTitle.setText(result);
+            }
+        };
+        MyTask myTask1 = new MyTask(onLoadCompletedListener);
+
+        myTask1.execute("http://docbao.vn");
+
+       // loadData();
+    }
+
+
+    public void loadData() {
+
+
+        try {
+            URL url = new URL("http://docbao.vn");
+
+            HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+
+            InputStream inputStream = httpURLConnection.getInputStream();
+
+            Scanner scanner = new Scanner(inputStream);
+
+            String data = "";
+
+
+            while (scanner.hasNext()) {
+
+                data = scanner.nextLine() + data;
+
+            }
+
+            scanner.close();
+
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
---
+++ app/src/main/java/com/d/asynctask/MyTask.java
@@ -0,0 +1,87 @@
+package com.d.asynctask;
+
+import android.os.AsyncTask;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
+
+public class MyTask extends AsyncTask<String, Long, String> {
+
+    public MyTask(TextView tvTitle) {
+        this.tvTitle = tvTitle;
+    }
+
+    private TextView tvTitle;
+
+    public MyTask() {
+
+    }
+
+
+    public interface OnLoadCompletedListener {
+        void onFinished(String result);
+    }
+
+
+    public MyTask(OnLoadCompletedListener onLoadCompletedListener) {
+        this.onLoadCompletedListener = onLoadCompletedListener;
+    }
+
+
+    public OnLoadCompletedListener onLoadCompletedListener;
+
+
+    // phuong thuc xu ly thread.
+    /// luu y :
+    @Override
+    protected String doInBackground(String... strings) {
+
+        try {
+            URL url = new URL(strings[0]);
+
+            HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+
+            InputStream inputStream = httpURLConnection.getInputStream();
+
+            Scanner scanner = new Scanner(inputStream);
+
+            String data = "";
+
+
+            while (scanner.hasNext()) {
+
+                data = scanner.nextLine() + data;
+
+            }
+
+            scanner.close();
+
+            return data;
+
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+
+    // ket thuc thread
+    @Override
+    protected void onPostExecute(String s) {
+        super.onPostExecute(s);
+
+        tvTitle.setText(s);
+
+        onLoadCompletedListener.onFinished(s);
+
+
+    }
+}
